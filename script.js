55946cd6ceda612be7c842a6649d6a8628a1e2ef
ahmedabdoaonew-max <ahmedabdoaonew@gmail.com>
2026-07-27 19:49:21 +0300

    Update certificate image URL for a student

--- script.js
+++ script.js
@@ -104,8 +104,8 @@ const studentsData = [
         lecturerName: 'د. محمد قطب', 
         courseName: 'التخاطب وعيوب الكلام', 
         lecturerImage: 'https://i.postimg.cc/rmH0t4S0/IMG-20260727-WA0037.jpg', 
-        certificateImage: 'https://i.postimg.cc/BnhKGRX5/IMG-20260727-192155.png' 
-    },
+        certificateImage: 'https://i.postimg.cc/BnhKGRX5/IMG-20260727-192155.png' , certificateImage:
+'https://i.postimg.cc/tRVwnjjv/IMG-20260727-194410.png' },
     { 
         code: 'SH-2025-002', 
         studentName: 'سارة خالد عبدالله', 
