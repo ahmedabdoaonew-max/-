401f6e84625743ead426eb7909544aebbb1897a5
ahmedabdoaonew-max <ahmedabdoaonew@gmail.com>
2026-08-28 15:51:32 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,12 @@
    بعد إنشاء المشروع فعّل: Authentication (Email/Password) + Firestore Database
    ============================================ */
 const firebaseConfig = {
-    apiKey: "ضع_API_KEY_هنا",
-    authDomain: "ضع_PROJECT_ID.firebaseapp.com",
-    projectId: "ضع_PROJECT_ID",
-    storageBucket: "ضع_PROJECT_ID.appspot.com",
-    messagingSenderId: "ضع_SENDER_ID",
-    appId: "ضع_APP_ID"
+    apiKey: "AIzaSyBCJUNaUO5LTjCu0koWu-RDGJh0uORJ2iE",
+    authDomain: "courses-68a1e.firebaseapp.com",
+    projectId: "courses-68a1e",
+    storageBucket: "courses-68a1e.firebasestorage.app",
+    messagingSenderId: "424796584661",
+    appId: "1:424796584661:web:1cceded7709c1643c0405c"
 };
 
 // تهيئة Firebase (يستخدم SDK النسخة المتوافقة compat - بدون أدوات بناء)
